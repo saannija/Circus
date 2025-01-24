1a4d8f966c2b1cdd0d29c50ad52b72e6d0754888
sa <saannija@gmail.com>
2025-01-24 09:01:11 +0200

    Buttons

---
+++ Assets/Scripts/FadeScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour
+{
+    Image img;
+    Color tempColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+            tempColor = img.color;
+            tempColor.a = 1f;
+            img.color = tempColor;
+        StartCoroutine(FadeOut(0.15f));
+    }
+
+    IEnumerator FadeOut (float seconds)
+    {
+        for (float a = 1f; a >= -0.05f; a -= 0.05f)
+        {
+            tempColor = img.color;
+            tempColor.a = a;
+            img.color = tempColor;
+            yield return new WaitForSeconds(seconds);
+        }
+        img.raycastTarget = false;
+    }
+    
+    IEnumerator FadeIn(float seconds)
+    {
+        img.raycastTarget = true;
+        for (float a = 0f; a <= 1.05f; a += 0.05f)
+        {
+            tempColor = img.color;
+            tempColor.a = a;
+            img.color = tempColor;
+            yield return new WaitForSeconds(seconds);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Unity 2.5D circus game project
 ### To do list:
 - [ ] Create main menu
 - [x] Change cursor
-- [ ] Add animation to dice and clouds
+- [x] Add animation to dice and clouds
 - [ ] Add sounds and music
 - [ ] Create character selection screen
 - [ ] Create settings menu
 - [ ] Create second screen
-...
+- [ ] Fix dice roll not working
