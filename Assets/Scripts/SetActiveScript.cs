f21d57d99f3bd42e5398be2b870fc32e8a2b3e8c
sa <saannija@gmail.com>
2025-02-11 15:11:57 +0200

    Main menu buttons work

--- Assets/Scripts/SetActiveScript.cs
+++ Assets/Scripts/SetActiveScript.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class SetActiveScript : MonoBehaviour
 {
-    public GameObject gameObject2;
-    public void ToggleActiveAfterDelay(float delay)
+    public GameObject buttonGroup;
+    public GameObject characterSelection;
+    public GameObject settingsButtons;
+    public GameObject leaderboardButtons;
+
+    //public GameObject gameObject2;
+    //public void ToggleActiveAfterDelay(float delay)
+    //{
+    //    StartCoroutine(ToggleActiveCoroutine(delay));
+    //}
+    //private IEnumerator ToggleActiveCoroutine(float delay)
+    //{
+    //    yield return new WaitForSeconds(delay);
+    //    gameObject2.SetActive(!gameObject2.activeSelf);
+    //    gameObject.SetActive(!gameObject.activeSelf);
+    //}
+    public void OpenCharacterSelection()
     {
-        StartCoroutine(ToggleActiveCoroutine(delay));
+        buttonGroup.SetActive(false);
+        characterSelection.SetActive(true);
     }
-    private IEnumerator ToggleActiveCoroutine(float delay)
+
+    public void OpenSettings()
     {
-        yield return new WaitForSeconds(delay);
-        gameObject2.SetActive(!gameObject2.activeSelf);
-        gameObject.SetActive(!gameObject.activeSelf);
+        buttonGroup.SetActive(false);
+        settingsButtons.SetActive(true);
     }
+
+    public void OpenLeaderboard()
+    {
+        buttonGroup.SetActive(false);
+        leaderboardButtons.SetActive(true);
+    }
+
+    public void CloseCharacterSelection()
+    {
+        buttonGroup.SetActive(true);
+        characterSelection.SetActive(false);
+    }
+
+    public void CloseSettings()
+    {
+        buttonGroup.SetActive(true);
+        settingsButtons.SetActive(false);
+    }
+
+    public void CloseLeaderboard()
+    {
+        buttonGroup.SetActive(true);
+        leaderboardButtons.SetActive(false);
+    }
+
 }
